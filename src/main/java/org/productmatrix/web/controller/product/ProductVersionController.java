8ecfd1d1a5335f9800130daabf72d29c36af4001
walkongrass <lizhu.zhanglz@gmail.com>
2013-01-05 23:24:39 +0800

    add fast json

--- src/main/java/org/productmatrix/web/controller/product/ProductVersionController.java
+++ src/main/java/org/productmatrix/web/controller/product/ProductVersionController.java
@@ -3,6 +3,10 @@
  */
 package org.productmatrix.web.controller.product;
 
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,6 +15,8 @@ import org.productmatrix.model.ProductVersion;
 import org.productmatrix.util.Paginer;
 import org.productmatrix.web.common.CommonRouterController;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.alibaba.fastjson.JSONObject;
 
 /**
  * @author lizhu.zhanglz
@@ -21,10 +27,35 @@ public class ProductVersionController extends CommonRouterController {
 	
 	public ModelAndView getProductVersions(HttpServletRequest request,HttpServletResponse response) throws Exception {
 		Long productId = Long.valueOf(request.getParameter("productId"));
-		int currentPage = Integer.valueOf(request.getParameter("currentPage"));
-		int pageSize = Integer.valueOf(request.getParameter("pageSize"));
+		int currentPage = Integer.valueOf(request.getParameter("page"));
+		int pageSize = Integer.valueOf(request.getParameter("rows"));
 		Paginer<ProductVersion> paginer = productVersionManager.listProductVersions(productId, currentPage, pageSize);
+		
+		StringBuilder builder = new StringBuilder("{\"total\":");
+		builder.append("\"").append(paginer.getTotal()).append("\",\"rows\":[");
+		List<ProductVersion> list=paginer.getContent();
+		for(int i = 0 , size = list.size(); i< size; i++){
+			builder.append(assembleProductVersion(list.get(i)));
+			if(size > 1 && i < size -1) {
+				builder.append(",");
+			}
+		}
+		builder.append("]}");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter writer = new PrintWriter(new OutputStreamWriter(response.getOutputStream(),"UTF8"),true);
+		writer.write(builder.toString());
+		writer.flush();
 		return null;
+	}
+
+	private String assembleProductVersion(ProductVersion productVersion) {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("versionid", productVersion.getVersionId());
+		jsonObject.put("versionnum", productVersion.getVersionNum());
+		jsonObject.put("desc", productVersion.getVersionDes());
+		jsonObject.put("gmtcreated", productVersion.getGmtCreated());
+		StringBuilder builder = new StringBuilder("{\"versionid\":\"");
+		return jsonObject.toJSONString();
 	}
 
 	public void setProductVersionManager(ProductVersionManager productVersionManager) {
