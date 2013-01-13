223ddc0630d86f54a84a7ef72260d8f13c214bb0
walkongrass <lizhu.zhanglz@gmail.com>
2013-01-13 13:53:41 +0800

    Add version info.

--- src/main/java/org/productmatrix/web/controller/product/ProductVersionController.java
+++ src/main/java/org/productmatrix/web/controller/product/ProductVersionController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.productmatrix.biz.product.ProductVersionManager;
 import org.productmatrix.model.ProductVersion;
+import org.productmatrix.util.DateUtil;
 import org.productmatrix.util.Paginer;
 import org.productmatrix.web.common.CommonRouterController;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,14 +31,20 @@ public class ProductVersionController extends CommonRouterController {
 		int currentPage = Integer.valueOf(request.getParameter("page"));
 		int pageSize = Integer.valueOf(request.getParameter("rows"));
 		Paginer<ProductVersion> paginer = productVersionManager.listProductVersions(productId, currentPage, pageSize);
+		if(paginer == null) {
+			paginer = new Paginer<ProductVersion>();
+			paginer.setTotal(0);
+		}
 		
 		StringBuilder builder = new StringBuilder("{\"total\":");
 		builder.append("\"").append(paginer.getTotal()).append("\",\"rows\":[");
-		List<ProductVersion> list=paginer.getContent();
-		for(int i = 0 , size = list.size(); i< size; i++){
-			builder.append(assembleProductVersion(list.get(i)));
-			if(size > 1 && i < size -1) {
-				builder.append(",");
+		if(paginer.getTotal() > 0){
+			List<ProductVersion> list=paginer.getContent();
+			for(int i = 0 , size = list.size(); i< size; i++){
+				builder.append(assembleProductVersion(list.get(i)));
+				if(size > 1 && i < size -1) {
+					builder.append(",");
+				}
 			}
 		}
 		builder.append("]}");
@@ -53,8 +60,7 @@ public class ProductVersionController extends CommonRouterController {
 		jsonObject.put("versionid", productVersion.getVersionId());
 		jsonObject.put("versionnum", productVersion.getVersionNum());
 		jsonObject.put("desc", productVersion.getVersionDes());
-		jsonObject.put("gmtcreated", productVersion.getGmtCreated());
-		StringBuilder builder = new StringBuilder("{\"versionid\":\"");
+		jsonObject.put("gmtcreated", DateUtil.format(productVersion.getGmtCreated()));
 		return jsonObject.toJSONString();
 	}
 
